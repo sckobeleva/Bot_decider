d20d5a6c66ec60d30219a89e783dcdf574bf5445
sckobeleva <64254934+sckobeleva@users.noreply.github.com>
2020-12-24 11:26:39 +0400

    Remove needless step, add help and url check

--- bot.py
+++ bot.py
@@ -7,36 +7,49 @@ bot = telebot.TeleBot(token)
 URL = "https://yesno.wtf/api"
 
 
-@bot.message_handler(commands=['start','help'])
+@bot.message_handler(commands=['start'])
 def start_command(message):
-    bot.send_message(message.chat.id, "Привет, я помогу тебе принять решение! Приступим?")
+    bot.send_message(message.chat.id, "Привет, я помогу тебе принять решение! Задай вопрос, на который можно ответить Да ✔ или Нет ❌")
+    bot.register_next_step_handler(message, show_answer)  # следующий шаг – функция show_answer
 
 
-@bot.message_handler(content_types=['text'])
-def ask_question(message):
-    if 'не' in message.text.lower() or 'no' in message.text.lower():
-        bot.send_message(message.chat.id, "Ну что ж, поговорим в другой раз!")
-        bot.register_next_step_handler(message, start_command)
-    else:
-        bot.send_message(message.from_user.id, "Задай вопрос, на который можно ответить 'Да' или 'Нет'.")
-        bot.register_next_step_handler(message, show_answer)  # следующий шаг – функция show_answer
+@bot.message_handler(commands=['help'])
+def help_command(message):
+    help = 'Привет, я решатель! \n\n' \
+           'Своим рождением я обязан API сервиса yesno.wtf. ' \
+            'Если вместо ответа ты видишь ошибку, значит, этот сервис временно не работает. ' \
+            'Обещаю, я начну свою работу сразу, как только смогу! :)'
+    bot.send_message(message.chat.id, text=help)
 
 
 @bot.message_handler(content_types=['text'])
 def show_answer(message):
-    if message.text[-1] != '?':
-        bot.send_message(message.from_user.id, "Я жду вопрос :)")
-        bot.register_next_step_handler(message, show_answer)
+    global URL
+    if check_url(URL) is False:
+        error = 'Извини, у меня выходной! Загляни пока в /help'
+        bot.send_message(message.from_user.id, error)
     else:
-        dict = load_info()
-        answer = dict.get("answer")
-        image = dict.get("image")
-        if answer == 'yes':
-            bot.send_message(message.from_user.id, 'Да!')
-            bot.send_message(message.from_user.id, image)
+        if message.text[-1] != '?':
+            bot.send_message(message.from_user.id, "❓ Я жду вопрос :)")
+            bot.register_next_step_handler(message, show_answer)
         else:
-            bot.send_message(message.from_user.id, 'Нет!')
-            bot.send_message(message.from_user.id, image)
+            dict = load_info()
+            answer = dict.get("answer")
+            image = dict.get("image")
+            if answer == 'yes':
+                bot.send_message(message.from_user.id, 'Да!')
+                bot.send_message(message.from_user.id, image)
+            else:
+                bot.send_message(message.from_user.id, 'Нет!')
+                bot.send_message(message.from_user.id, image)
+
+
+# вспомогательная функция, проверяет корректность ответа URL-адреса
+def check_url(URL):
+    try:
+        requests.head(URL)
+    except:
+        return False
 
 
 # вспомогательная функция, загружает информацию по указанному URL-адресу и возвращает их в формате словаря
